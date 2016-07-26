7c5ef1505f814dda14aa1d7eacb589c9f7ce8c5c
root <root@shawkin.com>
2016-07-25 21:38:25 -0400

    update

--- app/main/views.py
+++ app/main/views.py
@@ -54,6 +54,7 @@ def index():
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
+    #return render_template('user.html',user=user)
     page = request.args.get('page', 1, type=int)
     pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
         page, per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
@@ -62,6 +63,16 @@ def user(username):
     return render_template('user.html', user=user, posts=posts,
                            pagination=pagination)
 
+@main.route('/myposts/<username>')
+@login_required
+def myposts(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    page = request.args.get('page',1,type=int)
+    pagination = user.posts.order_by(Post.timestamp.desc()).paginate(
+        page,per_page=current_app.config['FLASKY_POSTS_PER_PAGE'],
+        error_out=False)
+    posts = pagination.items
+    return render_template('myposts.html',user=user,posts=posts,pagination=pagination)
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
 @login_required
